7463b236b632866b1c7d1312238eb463d3f63a77
YasmimVieira <vieiray937@gmail.com>
2022-09-24 01:53:24 -0300

    test: add first suite testing

---
+++ index.js
@@ -0,0 +1,8 @@
+const somaHorasExtras = (salario, valorHoraExtra) => salario + valorHoraExtra;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+export {
+  somaHorasExtras,
+  calculaDescontos,
+};
